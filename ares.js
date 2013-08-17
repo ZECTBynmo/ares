30be7cba69c8385778fb8d36cdfe8ccaff0a2103
ZECTBynmo <michael.vegeto@gmail.com>
2013-08-17 17:08:52 -0400

    Go back to what we had before

--- ares.js
+++ ares.js
@@ -1,4 +1,10 @@
-exports.ares = function( strCommand, debugOn, callback ) {
+function log( str ) { 
+	if( debugOn!=undefined ) {
+		console.log(str);
+	} 
+}
+
+function Ares( strCommand, debugOn, callback ) {
 	var log = function(str) { 
 		if(debugOn!=undefined) {
 			console.log(str);
@@ -8,21 +14,27 @@ exports.ares = function( strCommand, debugOn, callback ) {
 	var childProcess = require('child_process'),
 		command;
 
-	command = childProcess.exec(strCommand, callback || function (error, stdout, stderr) {
-		if (error) {
-			log(error.stack);
-			log('Error code: '+error.code);
-			log('Signal received: '+error.signal);
-		}
+	command = childProcess.exec(strCommand, callback);
 
-		if( stdout != undefined && stdout != null && stdout != "" )
-			log('Child Process STDOUT: ' + stdout);
-		
-		if( stderr != undefined && stderr != null && stderr != "" )
-			log('Child Process STDERR: ' + stderr);
+	command.on('data', function(data) {
+		console.log( data );
 	});
 
 	command.on('exit', function (code) {
 		log('Child process exited with exit code '+code);
 	});
+
+	return this;
 }
+
+//////////////////////////////////////////////////////////////////////////
+// Stops the proces
+Ares.prototype.kill = function( message ) {
+	try {
+		this.command.kill();
+	} catch( err ) {
+		console.log( "Error killing process: " + err );
+	}
+} // end kill()
+
+exports.ares = Ares;
