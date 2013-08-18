3d95edede8b88536edd277fc96f9f4a35f6bad90
ZECTBynmo <michael.vegeto@gmail.com>
2013-08-17 20:22:12 -0400

    add a fallback callback :)

--- ares.js
+++ ares.js
@@ -8,7 +8,19 @@ function Ares( strCommand, debugOn, callback ) {
 	var childProcess = require('child_process'),
 		command;
 
-	command = childProcess.exec(strCommand, callback);
+	command = childProcess.exec(strCommand, callback || function (error, stdout, stderr) {
+		if (error) {
+			log(error.stack);
+			log('Error code: '+error.code);
+			log('Signal received: '+error.signal);
+		}
+
+		if( stdout != undefined && stdout != null && stdout != "" )
+			log('Child Process STDOUT: ' + stdout);
+
+		if( stderr != undefined && stderr != null && stderr != "" )
+			log('Child Process STDERR: ' + stderr);
+	});
 
 	command.on('data', function(data) {
 		console.log( data );
